018c49684081a4f8628672306bf7e08ae3ef66ab
robolokbo <eldlakham@gmail.com>
2024-08-22 17:07:44 -0700

    Update Program5.java
    completed version as of 8/22/2024

--- Program5.java
+++ Program5.java
@@ -3,23 +3,36 @@
 //Your code here
 public class Program {
     public static void main(String[] args) {
-        double royaleMpg = (289/9.0)*10;
-        royaleMpg = (int) royaleMpg/10.0;
-        
-        double koopaMpg = (412/40.0)*10;
-        koopaMpg = (int) koopaMpg/10.0;
-        
-        double pipeMpg = (361/18.0)*10;
-        pipeMpg = (int) pipeMpg/10.0;
-        
-        double badwagonMpg = (161/11.0)*10;
-        badwagonMpg = (int) badwagonMpg/10.0;
-        
+
+        //declare variables
+        int royMi = 286;
+        int royGal = 9;
+        double royMpg = 0;
+
+        int kooMi = 412;
+        int kooGal = 40;
+        double kooMpg = 0;
+
+        int pipMi = 361;
+        int pipGal = 18;
+        double pipMpg = 0;
+
+        int badMi = 161;
+        int badGal = 11;
+        double badMpg = 0;
+
+        //calculate
+        royMpg = ((double)royMi / royGal)*10;
+        kooMpg = ((double)kooMi / kooGal)*10;
+        pipMpg = ((double)pipMi / pipGal)*10;
+        badMpg = ((double)badMi / badGal)*10;
+
+
         System.out.print("Mushroom Cup Prix Racer Average Miles/Per Gallon:\n\n");
-        System.out.println("Royale averaged " + royaleMpg + " m/g");
-        System.out.println("Koopa King averaged " + koopaMpg + " m/g");
-        System.out.println("Pipe Frame averaged " + pipeMpg + " m/g");
-        System.out.println("Badwagon averaged " + badwagonMpg + " m/g");
+        System.out.println("Royale averaged " + (int)royMpg/10.0 + " m/g");
+        System.out.println("Koopa King averaged " + (int)kooMpg/10.0 + " m/g");
+        System.out.println("Pipe Frame averaged " + (int)pipMpg/10.0 + " m/g");
+        System.out.println("Badwagon averaged " + (int)badMpg/10.0 + " m/g");
     }
 }
 //Paste console output below:
@@ -27,7 +40,7 @@ public class Program {
 
 Mushroom Cup Prix Racer Average Miles/Per Gallon:
 
-Royale averaged 32.1 m/g
+Royale averaged 31.7 m/g
 Koopa King averaged 10.3 m/g
 Pipe Frame averaged 20.0 m/g
 Badwagon averaged 14.6 m/g
